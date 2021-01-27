2467070c37baaa1b43ed6785540819de9e3f47f3
Yash-Netankar <netankaryash2002@gmail.com>
2021-01-27 10:24:59 +0530

    Solved the bug in library app

--- Banking System/index.js
+++ Banking System/index.js
@@ -1,54 +1,127 @@
-let account_form = document.getElementById("account_form");
-let uname = document.getElementById("name");
-let pass = document.getElementById("pass");
-let acc_type = document.getElementById("acc_type");
-let opening_balance = document.getElementById("opening_balance");
-let error = document.getElementById("error_div");
+// global 
+let input_form = document.getElementById("input_form");
+let Bname = document.getElementById("bname");
+let Bauthor = document.getElementById("bauthor");
 
-// creating the account
-account_form.addEventListener("submit", (e) => {
+
+// init book class
+class Book {
+    constructor(name, author, type) {
+        this.name = name;
+        this.author = author;
+        this.type = type;
+    }
+    static NotFound = () => {
+        let notFound = document.createElement("h1");
+        let no_record = document.querySelector(".no-record");
+        let text = document.createTextNode("No Records Found");
+        notFound.appendChild(text);
+        notFound.setAttribute("style", "text-align:center");
+        notFound.setAttribute("id", "not_found");
+        no_record.appendChild(notFound);
+    }
+}
+
+// delete book function
+const deleteB = (id) => {
+    let book = localStorage.getItem("Book");
+    if (book == "") {
+        arr = []
+    }
+    else {
+        arr = JSON.parse(book);
+    }
+    arr.splice(id, 1);
+    localStorage.setItem("Book", JSON.stringify(arr));
+    if (arr.length == 0) {
+        Book.NotFound();
+        document.getElementById("tableBody").innerHTML = "";
+    }
+    else {
+        Display.display();
+    }
+}
+
+// display book class
+class Display {
+    // display
+    static display = () => {
+        let tbody = document.getElementById("tableBody");
+        tbody.innerHTML = '';
+        let book = JSON.parse(localStorage.getItem("Book"));
+        if (book == null || book == "" || book.length == 0 || book == []) {
+            localStorage.clear();
+        }
+        let html = "";
+        try {
+            book.forEach((item, index) => {
+                html += `
+                <tr>
+                    <td>${index + 1}</td>
+                    <td>${item.name}</td>
+                    <td>${item.author}</td>
+                    <td>${item.type}</td>
+                    <td>
+                        <i class="fa fa-trash-o del_icon" id=${index} onclick="deleteB(this.id)"></i>
+                    </td>
+                </tr>`;
+            });
+            tbody.insertAdjacentHTML("beforeend", html);
+            if (document.getElementById("not_found")) {
+                document.getElementById("not_found").remove();
+            }
+        }
+        catch {
+            Book.NotFound();
+        }
+    }
+    // validation
+    static validate = () => {
+        if (Bname.value.length < 2 || Bauthor.value.length < 2 || Bname == "" || Bauthor == "") {
+            return false
+        }
+        else { return true }
+    }
+    // clear after submit
+    static Clear = () => {
+        input_form.reset();
+    }
+}
+Display.display();
+
+
+
+// playing with data and forms
+input_form.addEventListener("submit", (e) => {
     e.preventDefault();
 
-    if (uname.value.length !== 0) {
-        if (pass.value.length >= 5) {
-            if (acc_type.value === "Savings" || acc_type.value === "Current") {
-                if (opening_balance.value >= 100) {
-                    let account = localStorage.getItem("Account");
-                    // create new account
-                    if (account == null) {
-                        let arr = [{
-                            name: uname.value,
-                            pass: pass.value,
-                            balance: opening_balance.value,
-                            type: acc_type.value,
-                            date: new Date().toDateString()
-                        }];
-                        localStorage.setItem("Account", JSON.stringify(arr));
-
-                    }
-                    else {
-                        error.innerText = "Your Account Already Exists. Maybe With Another Name";
-                    }
-                }
-                else error.innerText = "Opening Balance Should Be Atleast 1000rs!!";
-            }
-            else error.innerText = "Enter Account Type!!";
+    // types
+    let type;
+    let types = document.querySelectorAll(".btype");
+    types.forEach(btn => {
+        if (btn.checked) {
+            type = btn.value;
         }
-        else error.innerText = "Enter Strong Password!!";
-    }
-    else error.innerText = "Please Enter Valid Name!!";
-})
+    });
+    // class object 1
+    let obj1 = new Book(Bname.value, Bauthor.value, type);
 
-let getData = () => {
-    fetch("https://api.github.com/users").then(res => {
-        return res.json();
-    }).then(data => {
-        data.forEach(element => {
-            console.log(`My Name is ${element.login}`);
-            console.log(`Follow Me By Clicking: ${element.url}`)
-        });
-    })
-}
-console.log("Getting data")
-getData()
-console.log("Got data")
+    // displaying on validation 
+    if (Display.validate()) {
+        // storing in local storage
+        let arr;
+        let book = localStorage.getItem("Book");
+        if (book == null) {
+            arr = [];
+        }
+        else {
+            arr = JSON.parse(book)
+        }
+        arr.push(obj1);
+        localStorage.setItem("Book", JSON.stringify(arr));
+        // displaying
+        Display.display()
+    }
+    else alert("Enter Valid Data \nCannot Record Your Entry");
+    Display.Clear();
+});
